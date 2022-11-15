570b9b5a1dfa773964ea322ff8495cda50ba5431
Joe Yates <joe.g.yates@gmail.com>
2022-11-15 12:16:15 +0100

    Set process-local byte order

--- lib/exiffer/binary.ex
+++ lib/exiffer/binary.ex
@@ -3,23 +3,47 @@ defmodule Exiffer.Binary do
   Documentation for `Exiffer.Binary`.
   """
 
+  @table :exiffer
+
+  def optionally_create_ets_table() do
+    if :ets.whereis(@table) do
+      _name = :ets.new(@table, [:set, :public, :named_table])
+    end
+  end
+
+  def set_byte_order(byte_order) do
+    optionally_create_ets_table()
+    :ets.insert(@table, {:byte_order, byte_order})
+  end
+
+  def byte_order() do
+    [byte_order: byte_order] = :ets.lookup(@table, :byte_order)
+    byte_order
+  end
+
   @doc """
   Convert binary bytes to decimal based on endianness.
   """
-  def endian_to_integer(<<hi, lo>>, :be) do
-    256 * hi + lo
+  def to_integer(binary) do
+    case byte_order() do
+      :little -> little_endian_to_integer(binary)
+      :big -> big_endian_to_integer(binary)
+    end
   end
 
-  def endian_to_integer(<<lo, hi>>, :le) do
-    lo + 256 * hi
-  end
-
-  def endian_to_integer(<<b0, b1, b2, b3>>, :be) do
-    0x1000000 * b0 + 0x10000 * b1 + 0x100 * b2 + b3
-  end
-
-  def endian_to_integer(<<b0, b1, b2, b3>>, :le) do
-    b0 + 0x100 * b1 + 0x10000 * b2 + 0x1000000 * b3
+  @doc """
+  Force big endian byte order
+  """
+  def big_endian(binary) do
+    case byte_order() do
+      :little ->
+        binary
+        |> :binary.bin_to_list()
+        |> Enum.reverse()
+        |> :binary.list_to_bin()
+      :big ->
+        binary
+    end
   end
 
   @doc """
@@ -27,6 +51,10 @@ defmodule Exiffer.Binary do
   """
   def big_endian_to_integer(<<hi, lo>>) do
     256 * hi + lo
+  end
+
+  def big_endian_to_integer(<<b0, b1, b2, b3>>) do
+    0x1000000 * b0 + 0x10000 * b1 + 0x100 * b2 + b3
   end
 
   @doc """
@@ -50,7 +78,7 @@ defmodule Exiffer.Binary do
   When given multiples of 8 bytes, returns a list of those tuples.
   """
   def to_rational(<<numerator::binary-size(4), denominator::binary-size(4)>>) do
-    {little_endian_to_integer(numerator), little_endian_to_integer(denominator)}
+    {to_integer(numerator), to_integer(denominator)}
   end
 
   def to_rational(<<rational1::binary-size(8), rational2::binary-size(8)>>) do
@@ -67,7 +95,7 @@ defmodule Exiffer.Binary do
   """
   def to_signed_rational(<<numerator::binary-size(4), denominator::binary-size(4)>>) do
     # TODO: handle signed
-    {little_endian_to_integer(numerator), little_endian_to_integer(denominator)}
+    {to_integer(numerator), to_integer(denominator)}
   end
 
   def to_signed_rational(<<rational1::binary-size(8), rational2::binary-size(8)>>) do
