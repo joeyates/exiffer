e4fc5a2c2080d15aac55199bad0f4c38bd3dc2b7
Joe Yates <joe.g.yates@gmail.com>
2022-11-22 17:39:21 +0100

    Set up bakeware release

--- mix.exs
+++ mix.exs
@@ -1,14 +1,17 @@
 defmodule Exiffer.MixProject do
   use Mix.Project
 
+  @app :exiffer
+
   def project do
     [
-      app: :exiffer,
+      app: @app,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      consolidate_protocols: Mix.env() != :test
+      consolidate_protocols: Mix.env() != :test,
+      releases: [{@app, release()}]
     ]
   end
 
@@ -22,6 +25,16 @@ defmodule Exiffer.MixProject do
   defp deps do
     [
       {:bakeware, ">= 0.0.0", runtime: false}
+    ]
+  end
+
+  defp release do
+    [
+      overwrite: true,
+      cookie: "#{@app}_cookie",
+      quiet: true,
+      steps: [:assemble, &Bakeware.assemble/1],
+      strip_beams: Mix.env() == :prod
     ]
   end
 end
