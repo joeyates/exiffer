edc29653613fefe796a2400df8ad9812f70280ea
Joe Yates <joe.g.yates@gmail.com>
2023-12-03 14:37:04 +0100

    Release

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Exiffer.MixProject do
   def project do
     [
       app: @app,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.14",
       description: "Read and update image metadata",
       package: package(),
