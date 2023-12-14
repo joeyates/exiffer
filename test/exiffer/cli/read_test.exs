0b0d2f8fbaccdcc3931c4d1f633be10dd1649438
Joe Yates <joe.g.yates@gmail.com>
2023-12-14 14:27:26 +0100

    Fix tests

--- test/exiffer/cli/read_test.exs
+++ test/exiffer/cli/read_test.exs
@@ -6,31 +6,31 @@ defmodule Exiffer.CLI.ReadTest do
 
   test "it reads JPEG files" do
     assert capture_io(fn ->
-      Read.run("test/support/fixtures/exiffer_code.jpg")
+      Read.run(%{filename: "test/support/fixtures/exiffer_code.jpg"})
     end) =~ "JPEG File Interchange Format\n"
   end
 
   test "it prints the JPEG version" do
     assert capture_io(fn ->
-      Read.run("test/support/fixtures/exiffer_code.jpg")
+      Read.run(%{filename: "test/support/fixtures/exiffer_code.jpg"})
     end) =~ "Version: 1.1\n"
   end
 
   test "it prints the JPEG X resolution" do
     assert capture_io(fn ->
-      Read.run("test/support/fixtures/exiffer_code.jpg")
+      Read.run(%{filename: "test/support/fixtures/exiffer_code.jpg"})
     end) =~ "X Resolution: 300\n"
   end
 
   test "it prints the JPEG Y resolution" do
     assert capture_io(fn ->
-      Read.run("test/support/fixtures/exiffer_code.jpg")
+      Read.run(%{filename: "test/support/fixtures/exiffer_code.jpg"})
     end) =~ "Y Resolution: 300\n"
   end
 
   test "it prints the JPEG comment" do
     assert capture_io(fn ->
-      Read.run("test/support/fixtures/exiffer_code.jpg")
+      Read.run(%{filename: "test/support/fixtures/exiffer_code.jpg"})
     end) =~ "Comment: Created with GIMP\n"
   end
 end
