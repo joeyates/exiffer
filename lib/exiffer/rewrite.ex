d27d6dd93e3e500ef1b828977420bc8a04235f23
Joe Yates <joe.g.yates@gmail.com>
2023-12-18 19:40:10 +0100

    Use Access behaviour to update

--- lib/exiffer/rewrite.ex
+++ lib/exiffer/rewrite.ex
@@ -10,32 +10,72 @@ defmodule Exiffer.Rewrite do
   alias Exiffer.JPEG.Header.APP1.EXIF
 
   def set_gps(%{} = input, %GPS{} = gps) do
-    Logger.info "Exiffer.Rewrite.set_gps/2"
-    {image, remainder} = Exiffer.parse(input)
-    Logger.info "Parsing complete"
+    Logger.info("Exiffer.Rewrite.set_gps/2")
+    {jpeg, remainder} = Exiffer.parse(input)
 
-    has_exif = has_exif?(image.headers)
-    metadata = if has_exif do
-      image.headers
-    else
-      [blank_exif() | image.headers]
-    end
-
-    Logger.info "Adding/updating GPS entry"
+    Logger.debug("Adding/updating GPS entry")
+    {headers, exif_index} = ensure_exif(jpeg.headers)
+    {headers, gps_index} = ensure_entry(headers, exif_index, :gps_info)
     entry = build_gps_entry(gps)
-    {:ok, metadata} = set_exif_ifd_entry(metadata, :gps_info, entry)
+    headers =
+      headers
+      |> update_in(
+        entries_path(exif_index) ++ [Access.at(gps_index)],
+        fn _existing -> entry end
+      )
 
-    Logger.info "Exiffer.Rewrite.set_gps/2 - complete"
-    {:ok, metadata, remainder}
+    {:ok, headers, remainder}
   end
 
-  defp has_exif?(headers) do
-    Enum.any?(headers, fn header -> header.__struct__ == EXIF end)
+  defp ensure_exif(headers) do
+    index = Enum.find_index(headers, fn header -> header.__struct__ == EXIF end)
+
+    if index do
+      {headers, index}
+    else
+      length = length(headers)
+      {headers ++ [blank_exif()], length}
+    end
   end
 
   defp blank_exif do
-    ifd_block = %IFDBlock{ifds: []}
-    %EXIF{byte_order: :little, ifd_block: ifd_block}
+    %EXIF{
+      byte_order: :little,
+      ifd_block: %IFDBlock{
+        ifds: [%IFD{entries: []}]
+      }
+    }
+  end
+
+  defp ensure_entry(headers, exif_index, type) do
+    index = entry_index(headers, exif_index, type)
+
+    if index do
+      {headers, index}
+    else
+      headers =
+        headers
+        |> update_in(
+          entries_path(exif_index),
+          fn entries -> [Entry.new_by_type(type, nil) | entries] end
+        )
+
+      {headers, 0}
+    end
+  end
+
+  defp entry_index(headers, exif_index, type) do
+    entries = ifd_entries(headers, exif_index)
+    Enum.find_index(entries, fn ifd -> ifd.type == type end)
+  end
+
+  defp ifd_entries(headers, exif_index) do
+    get_in(headers, entries_path(exif_index))
+  end
+
+  # We assume there is only one IFD in the EXIF block
+  defp entries_path(exif_index) do
+    [Access.at(exif_index), Access.key(:ifd_block), Access.key(:ifds), Access.at(0), Access.key(:entries)]
   end
 
   defp build_gps_entry(gps) do
@@ -55,6 +95,7 @@ defmodule Exiffer.Rewrite do
         Entry.new_by_type(:gps_altitude, {altitude, 1})
       ]
     }
+
     Entry.new_by_type(:gps_info, value)
   end
 
@@ -71,70 +112,5 @@ defmodule Exiffer.Rewrite do
   defp dms_to_rational({d, m, s}) do
     mus = floor(s * 1_000_000)
     [{d, 1}, {m, 1}, {mus, 1_000_000}]
-  end
-
-  defp set_exif_ifd_entry(headers, type, entry) do
-    {:ok, headers} = remove_exif_ifd_entry(headers, type)
-    {:ok, _headers} = add_exif_ifd_entry(headers, entry)
-  end
-
-  defp add_exif_ifd_entry(headers, entry) when is_list(headers) do
-    headers = Enum.map(headers, &(add_exif_ifd_entry(&1, entry)))
-    {:ok, headers}
-  end
-
-  defp add_exif_ifd_entry(%EXIF{} = exif, entry) do
-    ifd_block = add_exif_ifd_entry(exif.ifd_block, entry)
-    struct!(exif, ifd_block: ifd_block)
-  end
-
-  defp add_exif_ifd_entry(%IFDBlock{ifds: []} = ifd_block, entry) do
-    ifd = %IFD{entries: []}
-    ifd = add_exif_ifd_entry(ifd, entry)
-    struct!(ifd_block, ifds: [ifd])
-  end
-
-  defp add_exif_ifd_entry(%IFDBlock{} = ifd_block, entry) do
-    [ifd | others] = ifd_block.ifds
-    ifd = add_exif_ifd_entry(ifd, entry)
-    struct!(ifd_block, ifds: [ifd | others])
-  end
-
-  defp add_exif_ifd_entry(%IFD{} = ifd, entry) do
-    entries = Enum.reverse([entry | Enum.reverse(ifd.entries)])
-    struct!(ifd, entries: entries)
-  end
-
-  defp add_exif_ifd_entry(item, _entry), do: item
-
-  defp remove_exif_ifd_entry(headers, type) when is_list(headers) do
-    headers = Enum.map(headers, &(remove_exif_ifd_entry(&1, type)))
-    {:ok, headers}
-  end
-
-  defp remove_exif_ifd_entry(%EXIF{} = exif, type) do
-    ifd_block = remove_exif_ifd_entry(exif.ifd_block, type)
-    struct!(exif, ifd_block: ifd_block)
-  end
-
-  defp remove_exif_ifd_entry(%IFDBlock{} = ifd_block, type) do
-    ifds = Enum.map(ifd_block.ifds, &(remove_exif_ifd_entry(&1, type)))
-    struct!(ifd_block, ifds: ifds)
-  end
-
-  defp remove_exif_ifd_entry(%IFD{} = ifd, type) do
-    entries =
-      ifd.entries
-      |> Enum.map(&(remove_exif_ifd_entry(&1, type)))
-      |> Enum.filter(&(&1))
-    struct!(ifd, entries: entries)
-  end
-
-  defp remove_exif_ifd_entry(%Entry{type: type}, type) do
-    nil
-  end
-
-  defp remove_exif_ifd_entry(item, _type) do
-    item
   end
 end
