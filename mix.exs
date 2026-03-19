373020573e1396fb5cc8de051e6f4081a95de863
Joe Yates <joe.g.yates@gmail.com>
2026-03-19 18:37:43 +0100

    Update mix.exs dependencies: replace bakeware with burrito

--- mix.exs
+++ mix.exs
@@ -20,21 +20,15 @@ defmodule Exiffer.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    application = [
+    [
+      mod: {Exiffer.CLI, []},
       extra_applications: [:logger]
     ]
-
-    exiffer_build_cli = System.get_env("EXIFFER_BUILD_CLI")
-    if exiffer_build_cli do
-      [{:mod, {Exiffer.CLI, []}} | application]
-    else
-      application
-    end
   end
 
   defp deps do
     [
-      {:bakeware, ">= 0.0.0", runtime: false, optional: true},
+      {:burrito, "~> 1.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:helpful_options, ">= 0.3.3"},
       {:jason, ">= 0.0.0"}
@@ -49,8 +43,12 @@ defmodule Exiffer.MixProject do
       overwrite: true,
       cookie: "#{@app}_cookie",
       quiet: true,
-      steps: [:assemble, &Bakeware.assemble/1],
-      strip_beams: Mix.env() == :prod
+      strip_beams: Mix.env() == :prod,
+      burrito: [
+        targets: [
+          linux: [os: :linux, cpu: :x86_64]
+        ]
+      ]
     ]
   end
 
